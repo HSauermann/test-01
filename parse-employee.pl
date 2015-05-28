03620f6227c82814185142d077781a91f770b1fb
HSauermann <H.Sauermann@gmail.com>
2015-05-28 08:34:42 -0700

    'added

---
+++ parse-employee.pl
@@ -0,0 +1,91 @@
+#!C:\Perl -w
+# =============================================================================
+#
+#  File Name        : parse-employee.pl
+#
+#  Version          : "@(#) parse-employee.pl"
+#
+#  Project Name     : 
+#
+#  Copyright        : 
+#
+#  Author           : H. Sauermann
+#
+#  Platform         : Windows7
+#
+#  Date written     : Mai 2015
+#
+#  Last Modification: 
+#
+#  Description      : 
+#
+#
+#
+#  Modification History:
+#
+#  Author          Date        Version         Comment
+#  =============== =========== =============== ===============================
+#
+#  End Modification History
+#
+# =============================================================================
+#
+
+require 5;						# requires Perl version 5 or higher
+
+
+
+#############
+##
+## Includes
+##
+
+use strict;
+use warnings;
+
+
+
+##############
+##
+## Constants
+##
+
+
+
+##############
+##
+## Variables
+##
+
+
+
+############################
+##
+## Function predefinitions
+##
+
+
+
+#########
+##
+## main
+##
+
+print( "Perl installation is ok ... \n" );
+
+
+
+exit 0;
+
+
+
+#########################
+##
+## Function definitions
+##
+
+
+
+
+
+# End of File
